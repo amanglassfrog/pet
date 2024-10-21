13663a503863896c2dcabe25a0aa535668f9d3c0
Shubham Kumar <shubham@glassfrog.design>
2024-10-21 17:37:02 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,29 +6,27 @@ import Newsletter from "@/components/Newsletter/Newsletter";
 import PetShampooSection from "@/components/Petproducts/Petproducts";
 import PetHealthSection from "@/components/Productsection/Productsection";
 import Image from "next/image";
+import Script from "next/script"; 
 
 export default function Home() {
   return (
     <>
       <head>
         <meta name="google-site-verification" content="sl2813YiVanE1sUEhBlAfCmb5oUcjaO-d7oNn_GQI7c" />
-         <script
-        async
+        
+<Script
         src="https://www.googletagmanager.com/gtag/js?id=G-E2K2R6FDCZ"
         strategy="afterInteractive"
-      ></script>
-
-      <script id="google-analytics" strategy="afterInteractive">
+        async
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-E2K2R6FDCZ', {
-            page_path: window.location.pathname,
-          });
+          gtag('config', 'G-E2K2R6FDCZ');
         `}
-      </script>
-
+      </Script>
       </head>
       <Navbar />
       <Banner />
