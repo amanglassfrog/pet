fba04080f900c496617c5a1da20d8484937075d5
Shubham Kumar <shubham@glassfrog.design>
2024-10-22 09:45:27 +0530

    changes update

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,8 +33,25 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-KT8NY70ZMK');
           `}
         </Script>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WN3NK4JX');
+          `}
+        </Script>
       </head>
       <body className={`${fuzzyBubbles.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WN3NK4JX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
